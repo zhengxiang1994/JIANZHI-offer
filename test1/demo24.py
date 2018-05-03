1ffde4ef25c28856c8018e7c9c1ac157a14e4414
zhengxiang <1209549574@qq.com>
2018-05-03 21:13:11 +0800

    submit3

--- test1/demo24.py
+++ test1/demo24.py
@@ -7,13 +7,42 @@ class TreeNode:
 
 
 class Solution:
+    def __init__(self):
+        self.result = []
+        self.temp = []
+
     # 返回二维列表，内部每个列表表示找到的路径
     def FindPath(self, root, expectNumber):
         # write code here
-        pass
+        if not root:
+            return self.result
+        self.temp.append(root.val)
+        expectNumber -= root.val
+        if expectNumber == 0 and root.left is None and root.right is None:
+            self.result.append(self.temp[:])    # python2.x deep copy
+        self.FindPath(root.left, expectNumber)
+        self.FindPath(root.right, expectNumber)
+        self.temp.pop(-1)
+        return self.result
 
 
 if __name__ == "__main__":
-    pass
+    s = Solution()
+    node0 = TreeNode(1)
+    node1 = TreeNode(2)
+    node2 = TreeNode(3)
+    node3 = TreeNode(4)
+    node4 = TreeNode(5)
+    node5 = TreeNode(3)
+    node6 = TreeNode(6)
+    node0.left = node1
+    node0.right = node2
+    node1.left = node3
+    node1.right = node4
+    node2.left = node5
+    node2.right = node6
+    print(s.FindPath(node0, 7))
+
+
 
 
