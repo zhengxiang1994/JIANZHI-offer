4266f940be696d5cde87a395cd3841768b8d4403
zhengxiang <1209549574@qq.com>
2018-06-25 15:56:18 +0800

    demo25

--- test1/demo25.py
+++ test1/demo25.py
@@ -1,24 +1,69 @@
+# -*- coding:utf-8 -*-
+class RandomListNode:
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
+
+
 class Solution:
-    def coder(self, ls):
-        count = 0
-        result = []
-        for i in range(len(ls)):
-            if ls[i] == 0:
-                count += 1
-                if count >= 16:
-                    result.append(0)
-                    result.append(15)
-                    count = 0
-            else:
-                result.append(ls[i])
-                result.append(count)
-                count = 0
-        if ls[-1] == 0:
-            result.append(0)
-            result.append(count-1)
-        return result
+    # 返回 RandomListNode
+    def Clone(self, pHead):
+        # write code here
+        if pHead is None:
+            return None
+        self.CloneNodes(pHead)
+        self.ConnectRandomNodes(pHead)
+        return self.ReConnectNodes(pHead)
+    
+    # 复制原始链表的任一节点N并创建新节点N',再把N'放在N的后面
+    def CloneNodes(self, pHead):
+        pNode = pHead
+        while pNode:
+            pCloned = RandomListNode(pNode.label)
+            pCloned.next = pNode.next
+            pNode.next = pCloned
+            pNode = pCloned.next
+    
+    # 如果原始链表上的节点N的random指向S,则对应的复制节点N'的random指向S'(S的下一个节点)
+    def ConnectRandomNodes(self, pHead):
+        pNode = pHead
+        while pNode:
+            pCloned = pNode.next
+            if pNode.random:
+                pCloned.random = pNode.random.next
+            pNode = pCloned.next
+    
+    # 把得到的链表拆成两个链表，奇数位置上的节点组成原链表，偶数位置上的节点组成复制链表
+    def ReConnectNodes(self, pHead):
+        pNode = pHead
+        pClonedHead = pHead.next
+        while pNode:
+            cloneNode = pNode.next
+            pNode.next = cloneNode.next
+            cloneNode.next = None if cloneNode.next is None else cloneNode.next.next
+            pNode = pNode.next
+        return pClonedHead
 
 
 if __name__ == "__main__":
+    p1 = RandomListNode(1)
+    p2 = RandomListNode(2)
+    p3 = RandomListNode(3)
+    p4 = RandomListNode(4)
+    p5 = RandomListNode(5)
+    p1.next = p2
+    p2.next = p3
+    p3.next = p4
+    p4.next = p5
+    p1.random = p3
+    p2.random = p5
+    p4.random = p2
+
     s = Solution()
-    print(s.coder([0, 0, 1, 0, 0, 0, 0, 3, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 9, 0, 0, 0]))
+    pClone = s.Clone(p1)
+    pNode = pClone
+    while pNode:
+        print(pNode.label)
+        pNode = pNode.next
+
